3de1531934323144bbc0ad8eb90f84a30417f692
FernandoPain <fernando.serna.pain@gmail.com>
2023-02-09 00:55:30 +0100

    9_feb

--- Assets/Scrits/Bullet_Control.cs
+++ Assets/Scrits/Bullet_Control.cs
@@ -9,11 +9,13 @@ public class Bullet_Control : MonoBehaviour
     private BulletType Type;
     private float Speed;
     private int Dir;
+    private Game_Manager Manager;
     
-    public void InitBullet(BulletType _type, float _speed)
+    public void InitBullet(BulletType _type, float _speed, Game_Manager _manager)
     {
         Type = _type;
         Speed = _speed;
+        Manager = _manager;
         if (_type == BulletType.PLAYER)
         {
             Dir = 1;
@@ -30,6 +32,30 @@ public class Bullet_Control : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector2.up * Speed * Time.deltaTime);
+         
+        transform.Translate(Vector2.up * Dir * Speed * Time.deltaTime);
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        switch (Type)
+        {
+            case BulletType.ENEMY:
+                if (other.tag == "Player")
+                {
+                    Manager.GetDamgePlayer();
+                    Destroy(gameObject);
+                }
+                break;
+            case BulletType.PLAYER:
+                if (other.tag == "Enemy")
+                {
+                    Manager.GetDamageEnemy(other.gameObject);
+                    Destroy(gameObject);
+                }
+                break;
+        }
+        
     }
 }
--- Assets/Scrits/Game_Manager.cs
+++ Assets/Scrits/Game_Manager.cs
@@ -9,9 +9,23 @@ public class Game_Manager : MonoBehaviour
     public Vector2 InitEnemiesPos;
     public float SpeedPlayer;
 
+    private int TotalLifes;
+
     private World_Creator.WorldProperties CurrentWorld;
 
-    private List<GameObject> SavedEnemies;
+    public class EnemyProperties
+    {
+        public GameObject EnemyObj;
+        public Enemy_Creator.EnemyProperties Enemy;
+
+        public EnemyProperties(GameObject _obj, Enemy_Creator.EnemyProperties _enemy)
+        {
+            EnemyObj = _obj;
+            Enemy = _enemy;
+        }
+    }
+
+    private List<EnemyProperties> SavedEnemies;
 
     private float EnemySpeed, TotalEnemySpeed;
 
@@ -34,7 +48,8 @@ public class Game_Manager : MonoBehaviour
     private void PrintStage()
     {
         //Imprimir Mundo
-        CurrentWorld = World_Creator.GetWorldById(1);
+        CurrentWorld = World_Creator.GetWorldById(0);
+        TotalLifes = CurrentWorld.Lives;
         
         
         //Imprimir Player
@@ -44,6 +59,7 @@ public class Game_Manager : MonoBehaviour
         Destroy(Player.GetComponent<MeshCollider>());
         StartCoroutine(AddCollider(Player));
         Player.GetComponent<Renderer>().enabled = false;
+        Player.tag = "Player";
 
         GameObject SpritePlayer = new GameObject("PlayerSprite");
         SpritePlayer.transform.SetParent(Player.transform);
@@ -53,7 +69,7 @@ public class Game_Manager : MonoBehaviour
 
         //Imprimir Enemigos
 
-        SavedEnemies = new List<GameObject>();
+        SavedEnemies = new List<EnemyProperties>();
         TotalEnemySpeed = 0.5f;
 
         for (int i = 0; i< CurrentWorld.EnemiesID.Count; i++)
@@ -72,6 +88,7 @@ public class Game_Manager : MonoBehaviour
                 //Clase Hoy ==================================================================================
 
                 NewEnemy.GetComponent<Renderer>().enabled = false;
+                NewEnemy.tag = "Enemy";
 
                 GameObject SpriteEnemy = new GameObject(NewEnemy.name);
                 SpriteEnemy.transform.SetParent(NewEnemy.transform);
@@ -88,10 +105,34 @@ public class Game_Manager : MonoBehaviour
 
                 NewEnemy.AddComponent<Anim_Control>().InitAnim(SpriteEnemy.GetComponent<SpriteRenderer>(),
                     new List<Sprite>(Resources.LoadAll<Sprite>(CurrentWorld.EnemiesID[i].ToString())), 0.5f);
-                SavedEnemies.Add(NewEnemy);
+                SavedEnemies.Add(new EnemyProperties(NewEnemy, TempEnemy));
             }
         }
 
+    }
+
+    private void ClearStage()
+    {
+        for(int i =0; i < SavedEnemies.Count; i++)
+        {
+
+            print(SavedEnemies.Count);
+            //Destroy(SavedEnemies[i].EnemyObj);
+        }
+        SavedEnemies = new List<EnemyProperties>();
+        
+        //Destroy(Player);
+
+        GameObject[] AllBullets = GameObject.FindGameObjectsWithTag("Bullet");
+        for (int i = 0; i < AllBullets.Length; i++)
+        {
+            if (AllBullets[i] != null)
+            {
+                print(AllBullets.Length);
+                //Destroy(AllBullets[i]);
+            } 
+            
+        }
     }
     IEnumerator AddCollider(GameObject _obj)
     {
@@ -109,7 +150,7 @@ public class Game_Manager : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            CreateBullet(BulletType.PLAYER, Player.transform.position, 30);
+            CreateBullet(BulletType.PLAYER, Player.transform.position, 30, Color.white);
         }
     }
 
@@ -123,16 +164,19 @@ public class Game_Manager : MonoBehaviour
                 switch (Direction)
                 {
                     case EnemyDir.RIGHT:
-                        SavedEnemies[i].transform.position =
-                            new Vector2(SavedEnemies[i].transform.position.x + 1, SavedEnemies[i].transform.position.y);
+                        SavedEnemies[i].EnemyObj.transform.position =
+                            new Vector2(SavedEnemies[i].EnemyObj.transform.position.x + 1, SavedEnemies[i].EnemyObj.transform.position.y);
                         break;
                     case EnemyDir.LEFT:
-                        SavedEnemies[i].transform.position =
-                            new Vector2(SavedEnemies[i].transform.position.x - 1, SavedEnemies[i].transform.position.y);
+                        SavedEnemies[i].EnemyObj.transform.position =
+                            new Vector2(SavedEnemies[i].EnemyObj.transform.position.x - 1, SavedEnemies[i].EnemyObj.transform.position.y);
                         break ;
 
                 }
             }
+            int RandomEnemy = Random.Range(0, SavedEnemies.Count);
+            Color EnemyColor = SavedEnemies[RandomEnemy].EnemyObj.transform.GetChild(0).GetComponent<SpriteRenderer>().color;
+            CreateBullet(BulletType.ENEMY, SavedEnemies[RandomEnemy].EnemyObj.transform.position, 10, EnemyColor);
             //Comprobar si algún enemigo ha llegado al límite de la pantalla
             CheckEnemyLimits();
             EnemySpeed = 0;
@@ -142,12 +186,12 @@ public class Game_Manager : MonoBehaviour
     {
         for(int i = 0; i < SavedEnemies.Count; i++)
         {
-            if(SavedEnemies[i].transform.position.x <= LimitsStage.x) //Moviendo a la izquierda
+            if(SavedEnemies[i].EnemyObj.transform.position.x <= LimitsStage.x) //Moviendo a la izquierda
             {
                 Direction = EnemyDir.RIGHT;
                 break;
             }
-            if(SavedEnemies[i].transform.position.x >= LimitsStage.y)
+            if(SavedEnemies[i].EnemyObj.transform.position.x >= LimitsStage.y)
             {
                 Direction=EnemyDir.LEFT;
                 break;
@@ -155,7 +199,7 @@ public class Game_Manager : MonoBehaviour
         }
         
     }
-    private void CreateBullet(BulletType _type, Vector2 _pos, float _speed)
+    private void CreateBullet(BulletType _type, Vector2 _pos, float _speed, Color _color)
     {
         GameObject NewBullet = GameObject.CreatePrimitive(PrimitiveType.Quad);
         NewBullet.name = "Bullet";
@@ -163,7 +207,49 @@ public class Game_Manager : MonoBehaviour
         NewBullet.transform.position = _pos;
         Destroy(NewBullet.GetComponent<MeshCollider>());
         StartCoroutine(AddCollider(NewBullet));
+        NewBullet.GetComponent<MeshRenderer>().enabled = false;
+        NewBullet.tag = "Bullet";
 
-        NewBullet.AddComponent<Bullet_Control>().InitBullet(_type, _speed);
+        GameObject SpriteBullet = new GameObject("Sprite Bullet");
+        SpriteBullet.transform.SetParent(NewBullet.transform);
+        SpriteBullet.transform.localScale = new Vector3(1f, 1f, 1f);
+        SpriteBullet.transform.localPosition = new Vector2(0, 0);
+        SpriteBullet.AddComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Bullet");
+        SpriteBullet.GetComponent<SpriteRenderer>().color = _color;
+        if(_type == BulletType.PLAYER)
+        {
+            SpriteBullet.transform.rotation = Quaternion.Euler(0, 0, 180);
+        }
+
+
+
+        NewBullet.AddComponent<Bullet_Control>().InitBullet(_type, _speed, this);
+        Destroy(NewBullet, 3);
     }
+
+    public void GetDamageEnemy(GameObject _obj)
+    {
+        for (int i = 0; i< SavedEnemies.Count; i++)
+        {
+            if (SavedEnemies[i].EnemyObj == _obj)
+            {
+                SavedEnemies[i].Enemy.Lives--;
+                if(SavedEnemies[i].Enemy.Lives <= 0)
+                {
+                    Destroy(SavedEnemies[i].EnemyObj);
+                    SavedEnemies.RemoveAt(i); 
+                }
+            }
+        }
+    }
+    public void GetDamgePlayer()
+    {
+        TotalLifes--;
+        if (TotalLifes<=0)
+        {
+            print("Fin Partida");
+            ClearStage();
+        }
+    }
+
 }
